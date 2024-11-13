78f7ef0802a39a7d737a8f0ac22efc55b0aed199
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-14 06:12:29 +0700

    add: .env file to protect connectionPool password leakage

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,9 +1,11 @@
 // Create PostgreSQL Connection Pool here !
 import * as pg from "pg";
 const { Pool } = pg.default;
+import "dotenv/config";
 
 const connectionPool = new Pool({
-  connectionString: "postgresql://postgres:16708824@localhost:5432/quoramock",
+  connectionString: process.env.CONNECTION_STRING,
 });
+// CONNECTION_STRING = "postgresql://postgres:16708824@localhost:5432/quoramock" อันนี้คือในตัวไฟล์ .env
 
 export default connectionPool;
